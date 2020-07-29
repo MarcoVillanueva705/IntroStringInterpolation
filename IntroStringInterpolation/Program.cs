a6589f977978371fee22620b409d4f60975553e1
marco villanueva <villanuevamarco98@gmail.com>
2020-07-28 20:14:48 -0600

    optimized interpolation note

--- IntroStringInterpolation/Program.cs
+++ IntroStringInterpolation/Program.cs
@@ -11,6 +11,8 @@ namespace IntroStringInterpolation
 
             //string interpolation doesn't have the same memory issues as
             //string concatenation
+            //best way to get expressions into our strings
+            //It's optimized and provides clean syntax
             Console.WriteLine($" x{point.x} y{point.y}"); 
         }
     }
