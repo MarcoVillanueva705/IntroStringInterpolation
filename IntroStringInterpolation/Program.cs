f058b454ff087c2fb03287861d7534dd3834735f
marco villanueva <villanuevamarco98@gmail.com>
2020-07-28 20:20:02 -0600

    concat note

--- IntroStringInterpolation/Program.cs
+++ IntroStringInterpolation/Program.cs
@@ -8,6 +8,12 @@ namespace IntroStringInterpolation
         {
             var point = new { x = 1, y = 2 };
             //concat exercise
+            //each time we concatenate a string into another
+            //it creates a new string object
+            //If done in a loop, it would provide a lot of
+            //unnecessary work for the garbage collector
+
+            Console.WriteLine("x" + point.x + "y" + point.y);
 
             //string interpolation doesn't have the same memory issues as
             //string concatenation
