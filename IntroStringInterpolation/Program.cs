03f36a10d6bb4224492ab773c2470ba9c8366f36
marco villanueva <villanuevamarco98@gmail.com>
2020-07-29 20:42:45 -0600

    working with formatters

--- IntroStringInterpolation/Program.cs
+++ IntroStringInterpolation/Program.cs
@@ -6,20 +6,30 @@ namespace IntroStringInterpolation
     {
         static void Main(string[] args)
         {
-            var point = new { x = 1, y = 2 };
+            //var point = new { x = 1, y = 2 };
             //concat exercise
             //each time we concatenate a string into another
             //it creates a new string object
             //If done in a loop, it would provide a lot of
             //unnecessary work for the garbage collector
 
-            Console.WriteLine("x" + point.x + "y" + point.y);
+            //Console.WriteLine("x" + point.x + "y" + point.y);
 
             //string interpolation doesn't have the same memory issues as
             //string concatenation
             //best way to get expressions into our strings
             //It's optimized and provides clean syntax
-            Console.WriteLine($" x{point.x} y{point.y}"); 
+
+            //working with formatters
+            //to limit the amt of decimals in a float
+            //use N1 after semicolon in var obj
+            var point = new {x = 1.1234, y = 2.1254 };
+
+            //Console.WriteLine($" x{point.x} y{point.y:N1}");
+            //adding margin or space between margin or string literal
+            //place number value in front of colon to indicate the amt
+            Console.WriteLine($" x{point.x} y{point.y,10:N1}");
+
         }
     }
 }
